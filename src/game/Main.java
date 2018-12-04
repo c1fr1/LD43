a55941da2100028a167b19de3a3a80f56eac854f
c1fr1 <c1fr1.stkcps@gmail.com>
2018-12-04 05:05:10 +0000

    fixed mac compatibility

--- src/game/Main.java
+++ src/game/Main.java
@@ -13,11 +13,10 @@ public class Main {
 	public static float gameOverOpacity = - 1;
 	public static SoundSource source;
 	public static void main(String[] args) {
-		
 		if (args.length == 0) {
 			String os = System.getProperty("os.name");
 			System.out.println("Operating System: " + os);
-			if (os.indexOf("mac") >= 0) {
+			if (os.indexOf("mac") >= 0 || os.indexOf("Mac") >= 0) {
 				System.out.println("in order to get a stack trace, run with\njava -jar 'Flaming Resistance.jar' noReRun -XstartOnFirstThread");
 				try {
 					Runtime.getRuntime().exec(new String[]{"java", "-XstartOnFirstThread", "-jar", "'Flaming Resistance.jar'", "noReRun"});
@@ -25,36 +24,26 @@ public class Main {
 					e.printStackTrace();
 				}
 			}else {
-				EnigWindow.runOpeningSequence = false;
-				EnigWindow window = new EnigWindow("Flaming Resistance", "res/textures/icon.png");
-				source = new SoundSource();
-				Shaders.createMainShaders();
-				screenObj = new VAO(-1, -1, 2, 2);
-				entityObj = new VAO(-5, -5, 10, 10);
-				titleObj = new VAO(-30, 10, 60, 30);
-				window.fps = 60;
-				MainView.main = new MainView(window);
-				MenuView.main = new MenuView(window);
-				AudioHandler.setup();
-				MenuView.main.runLoop();
-				AudioHandler.timer.cancel();
-				window.terminate();
+				runGame();
 			}
 		}else if (args[0].equals("noReRun")) {
-			EnigWindow.runOpeningSequence = false;
-			EnigWindow window = new EnigWindow("Flaming Resistance", "res/textures/icon.png");
-			source = new SoundSource();
-			Shaders.createMainShaders();
-			screenObj = new VAO(-1, -1, 2, 2);
-			entityObj = new VAO(-5, -5, 10, 10);
-			titleObj = new VAO(-30, 10, 60, 30);
-			window.fps = 60;
-			MainView.main = new MainView(window);
-			MenuView.main = new MenuView(window);
-			AudioHandler.setup();
-			MenuView.main.runLoop();
-			AudioHandler.timer.cancel();
-			window.terminate();
+			runGame();
 		}
+	}
+	public static void runGame() {
+		EnigWindow.runOpeningSequence = false;
+		EnigWindow window = new EnigWindow("Flaming Resistance", "res/textures/icon.png");
+		source = new SoundSource();
+		Shaders.createMainShaders();
+		screenObj = new VAO(-1, -1, 2, 2);
+		entityObj = new VAO(-5, -5, 10, 10);
+		titleObj = new VAO(-30, 10, 60, 30);
+		window.fps = 60;
+		MainView.main = new MainView(window);
+		MenuView.main = new MenuView(window);
+		AudioHandler.setup();
+		MenuView.main.runLoop();
+		AudioHandler.timer.cancel();
+		window.terminate();
 	}
 }
